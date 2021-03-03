99c5f9b921c7369bfa698f72a6ed10b0fbfaa484
tacodes <64085493+tim-andes@users.noreply.github.com>
2021-03-03 12:04:51 -0500

    Update test_unit.py

--- test_unit.py
+++ test_unit.py
@@ -1,4 +1,12 @@
 import pytest
 import phoneemailextractor
 
-# TODO add unit tests for regexes, file writing, matching, producing
+# TODO add unit tests
+
+# test - phone number regex
+
+# test - email address regex
+
+# test - finder
+
+# test - file writing
